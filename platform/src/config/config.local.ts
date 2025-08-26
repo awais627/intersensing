6a21df7eeee1adf701e75d069afb968765a49693
omair <omair445@live.com>
2025-08-26 15:00:29 +0500

    setup

---
+++ platform/src/config/config.local.ts
@@ -0,0 +1,10 @@
+export const AppConfig = {
+	ENV: 'dev',
+	APP_BASE_URL: 'https://localhost:3000'
+}
+
+export const ApiConfig = {
+	API_BASE_URL: 'http://localhost:9000',
+	API_WEBSOCKET_URL: 'http://localhost:9003',
+	API_CFC_URL: 'http://localhost:9091'
+}
