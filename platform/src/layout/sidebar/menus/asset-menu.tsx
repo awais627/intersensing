08f3a92887c6f7bc298389ff9f6a933a15bf6e88
omair <omair445@live.com>
2025-08-28 14:10:20 +0500

    alerts page

--- platform/src/layout/sidebar/menus/asset-menu.tsx
+++ platform/src/layout/sidebar/menus/asset-menu.tsx
@@ -2,52 +2,28 @@ import { RiDonutChartLine, RiFolderChartLine } from 'react-icons/ri'
 import { GTransition } from 'components/basic-blocks'
 import { NavItem } from 'layout/sidebar/nav-item'
 
-import { Disclosure } from '@headlessui/react'
-
 export const AssetMenu = () => {
 	return (
 		<GTransition show={true}>
 			<div>
 				<div>
-					<Disclosure as="div" className="pt-4 pl-2" defaultOpen={true}>
-						<Disclosure.Button
-							className={`group flex w-full items-center justify-between hover:bg-gray-50 ${'bg-gray-100 rounded-md'}`}
-						>
-							<span className="text-sm/6 font-medium text-white group-aria-[hover]:text-white/80">
-								<NavItem
-									label="Dashboard"
-									to={() => `/dashboard`}
-									icon={RiDonutChartLine}
-								/>
-							</span>
-						</Disclosure.Button>
-					</Disclosure>
-					<Disclosure as="div" className="pt-4 pl-2">
-						<Disclosure.Button
-							className={`group flex w-full items-center justify-between hover:bg-gray-50 ${'bg-gray-100 rounded-md'}`}
-						>
-							<span className="text-sm/6 font-medium text-white group-aria-[hover]:text-white/80">
-								<NavItem
-									label="Telemetry"
-									to={() => `/telemetry`}
-									icon={RiDonutChartLine}
-								/>
-							</span>
-						</Disclosure.Button>
-					</Disclosure>
-					<Disclosure as="div" className="pt-4 pl-2">
-						<Disclosure.Button
-							className={`group flex w-full items-center justify-between hover:bg-gray-50 ${'bg-gray-100 rounded-md'}`}
-						>
-							<span className="text-sm/6 font-medium text-white group-aria-[hover]:text-white/80">
-								<NavItem
-									label="Alerts"
-									to={() => `/alerts`}
-									icon={RiDonutChartLine}
-								/>
-							</span>
-						</Disclosure.Button>
-					</Disclosure>
+					<div className="pl-2">
+						<NavItem
+							label="Dashboard"
+							to={() => `/dashboard`}
+							icon={RiDonutChartLine}
+						/>
+						<NavItem
+							label="Telemetry"
+							to={() => `/telemetry`}
+							icon={RiDonutChartLine}
+						/>
+						<NavItem
+							label="Alerts"
+							to={() => `/alerts`}
+							icon={RiDonutChartLine}
+						/>
+					</div>
 					<div className="pl-2">
 						<NavItem
 							className="reports-tab"
