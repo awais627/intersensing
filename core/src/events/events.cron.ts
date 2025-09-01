5c426c94b2233aa88b90b5f57cca1aa2942537a2
omair <omair445@live.com>
2025-09-01 16:41:50 +0500

    mqtt setup

--- core/src/events/events.cron.ts
+++ core/src/events/events.cron.ts
@@ -1,5 +1,6 @@
 import { Injectable, Logger } from "@nestjs/common";
 import { EventsService } from "./events.service";
+import { Cron, CronExpression } from "@nestjs/schedule";
 
 @Injectable()
 export class EventsCronService {
@@ -8,25 +9,25 @@ export class EventsCronService {
 
   constructor(private readonly eventsService: EventsService) {}
 
-  // @Cron(CronExpression.EVERY_5_SECONDS) // Every 2 seconds
-  // async handleCron() {
-  //   this.jobCounter++;
-  //
-  //   try {
-  //     this.logger.log(
-  //       `Cron job #${this.jobCounter} - Adding mock data to queue`,
-  //     );
-  //
-  //     // Generate mock data job
-  //     await this.eventsService.generateMockData();
-  //
-  //     this.logger.log(
-  //       `Cron job #${this.jobCounter} - Mock data job added successfully`,
-  //     );
-  //   } catch (error) {
-  //     this.logger.error(
-  //       `Cron job #${this.jobCounter} - Failed to add mock data: ${error.message}`,
-  //     );
-  //   }
-  // }
+  @Cron(CronExpression.EVERY_5_SECONDS) // Every 2 seconds
+  async handleCron() {
+    this.jobCounter++;
+
+    try {
+      this.logger.log(
+        `Cron job #${this.jobCounter} - Adding mock data to queue`,
+      );
+
+      // Generate mock data job
+      await this.eventsService.generateMockData();
+
+      this.logger.log(
+        `Cron job #${this.jobCounter} - Mock data job added successfully`,
+      );
+    } catch (error) {
+      this.logger.error(
+        `Cron job #${this.jobCounter} - Failed to add mock data: ${error.message}`,
+      );
+    }
+  }
 }
